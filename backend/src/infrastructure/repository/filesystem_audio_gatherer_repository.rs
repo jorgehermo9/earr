03dac66d7edce1a9116698e02fa18b3021bb3639
Jorge Hermo <jorgehermogonzalez@gmail.com>
2023-12-03 16:39:46 +0100

    fast committed

--- backend/src/infrastructure/repository/filesystem_audio_gatherer_repository.rs
+++ backend/src/infrastructure/repository/filesystem_audio_gatherer_repository.rs
@@ -35,17 +35,23 @@ impl AudioGathererRepository for FilesystemAudioGathererRepository {
         let audio_iter = walker
             .flatten()
             .filter(|entry| entry.file_type().is_file())
-            .filter_map(|entry| match Audio::try_from(entry) {
-                Ok(entry) => Some(entry),
-                Err(err) => {
-                    eprintln!("Failed to read entry: {}", err);
-                    None
-                }
+            .filter_map(|entry| {
+                Audio::try_from(entry)
+                    .map_err(|e| {
+                        eprintln!("Failed to read audio: {}", e);
+                    })
+                    .ok()
             });
 
         Ok(Box::new(audio_iter))
     }
 }
+
+const DEFAULT_AUDIO_TITLE: &str = "UNKNOWN";
+const DEFAULT_AUDIO_ARTIST: &str = "UNKNOWN";
+const DEFAULT_AUDIO_ALBUM_TITLE: &str = "UNKNOWN";
+const DEFAULT_AUDIO_ALBUM_ARTIST: &str = "UNKNOWN";
+const DEFAULT_AUDIO_GENRE: &str = "UNKNOWN";
 
 impl TryFrom<walkdir::DirEntry> for Audio {
     type Error = FilesystemAudioGathererRepositoryError;
@@ -55,50 +61,53 @@ impl TryFrom<walkdir::DirEntry> for Audio {
         // function
         let audio_tags = Tag::new().read_from_path(entry_path)?;
 
-        // Title
-        let default_title = entry_path
+        let (mut default_artist, mut default_title) = entry_path
             .file_stem()
             .and_then(|stem| stem.to_str())
-            // Check if there is a '-' in the file name
-            .and_then(|stem| stem.find('-').map(|_| stem))
+            .map(|stem| stem.trim())
             // Split by '-' and take the second part as title
-            .and_then(|stem| stem.split('-').nth(1))
-            .unwrap_or("UNKNOWN");
+            .and_then(|stem| stem.split_once(" - "))
+            .map(|(artist, title)| (artist.trim(), title.trim()))
+            .unwrap_or((DEFAULT_AUDIO_TITLE, DEFAULT_AUDIO_ARTIST));
+
+        // If the artist is a number, it's probably the track number
+        if default_artist.is_empty() || default_artist.parse::<u32>().is_ok() {
+            default_artist = DEFAULT_AUDIO_TITLE;
+        }
+
+        if default_title.is_empty() {
+            default_title = DEFAULT_AUDIO_ARTIST;
+        }
+
+        // Title
         let title = audio_tags.title().unwrap_or(default_title).to_string();
 
         // Artist
-        let default_artist = entry_path
-            .file_stem()
-            .and_then(|stem| stem.to_str())
-            // Check if there is a '-' in the file name
-            .and_then(|stem| stem.find('-').map(|_| stem))
-            // Split by '-' and take the first part as artist
-            .and_then(|stem| stem.split('-').nth(0))
-            // Check if it is a number and return none if it is. It may be related to track number
-            .and_then(|artist| {
-                if artist.parse::<u32>().is_ok() {
-                    None
-                } else {
-                    Some(artist)
-                }
-            })
-            .unwrap_or("UNKNOWN");
         let artist = audio_tags.artist().unwrap_or(default_artist).to_string();
 
         // Year
         let year = audio_tags.year();
 
         // Album title
-        let album_title = audio_tags.album_title().unwrap_or("UNKNOWN").to_string();
+        let album_title = audio_tags
+            .album_title()
+            .unwrap_or(DEFAULT_AUDIO_ALBUM_TITLE)
+            .to_string();
 
         // Album artist
-        let album_artist = audio_tags.album_artist().unwrap_or("UNKNOWN").to_string();
+        let album_artist = audio_tags
+            .album_artist()
+            .unwrap_or(DEFAULT_AUDIO_ALBUM_ARTIST)
+            .to_string();
 
         // Album cover
         let album_cover = audio_tags.album_cover().map(|cover| cover.data.to_vec());
 
         // Genre
-        let genre = audio_tags.genre().unwrap_or("UNKNOWN").to_string();
+        let genre = audio_tags
+            .genre()
+            .unwrap_or(DEFAULT_AUDIO_GENRE)
+            .to_string();
 
         let audio = AudioBuilder::default()
             .title(title)
